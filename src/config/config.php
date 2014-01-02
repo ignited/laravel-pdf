4540a9560ff5110f92b54717a956c18c1c41de2b
rezen <an2dres5m@gmail.com>
2014-01-02 14:31:47 -0800

    Update config to make Windows happy
    Also part of fix for https://github.com/ignited/laravel-pdf/issues/2

--- src/config/config.php
+++ src/config/config.php
@@ -2,10 +2,10 @@
 
 return array(
 	# Uncomment for 32-bit systems
-	#'bin' => base_path() . '/vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386'
+	#'binPath' => base_path() . '/vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386'
 	
 	# Uncomment for 64-bit systems
-	#'bin' => base_path() . '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'
+	#'binPath' => base_path() . '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'
 
 	# Add any global Wkhtmltopdf options here
 	#'no-outline',
